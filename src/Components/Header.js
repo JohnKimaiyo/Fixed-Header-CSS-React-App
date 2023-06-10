46216fee64302aa64ef778bd4adbcead00630d90
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-06-10 18:05:33 +0300

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <h1>Fixed Angled Header</h1>
+      </header>
+      <main>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          dolor accusamus neque laborum atque accusantium perspiciatis, in illo
+          itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique
+          facere unde quos necessitatibus quia culpa eligendi consequuntur vero
+          quas eum dolorem voluptates laboriosam? Dolor neque quas minus numquam
+          laboriosam magni voluptatem enim eveniet.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          dolor accusamus neque laborum atque accusantium perspiciatis, in illo
+          itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique
+          facere unde quos necessitatibus quia culpa eligendi consequuntur vero
+          quas eum dolorem voluptates laboriosam? Dolor neque quas minus numquam
+          laboriosam magni voluptatem enim eveniet.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          dolor accusamus neque laborum atque accusantium perspiciatis, in illo
+          itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique
+          facere unde quos necessitatibus quia culpa eligendi consequuntur vero
+          quas eum dolorem voluptates laboriosam? Dolor neque quas minus numquam
+          laboriosam magni voluptatem enim eveniet.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          dolor accusamus neque laborum atque accusantium perspiciatis, in illo
+          itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique
+          facere unde quos necessitatibus quia culpa eligendi consequuntur vero
+          quas eum dolorem voluptates laboriosam? Dolor neque quas minus numquam
+          laboriosam magni voluptatem enim eveniet.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          dolor accusamus neque laborum atque accusantium perspiciatis, in illo
+          itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique
+          facere unde quos necessitatibus quia culpa eligendi consequuntur vero
+          quas eum dolorem voluptates laboriosam? Dolor neque quas minus numquam
+          laboriosam magni voluptatem enim eveniet.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          dolor accusamus neque laborum atque accusantium perspiciatis, in illo
+          itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique
+          facere unde quos necessitatibus quia culpa eligendi consequuntur vero
+          quas eum dolorem voluptates laboriosam? Dolor neque quas minus numquam
+          laboriosam magni voluptatem enim eveniet.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          dolor accusamus neque laborum atque accusantium perspiciatis, in illo
+          itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique
+          facere unde quos necessitatibus quia culpa eligendi consequuntur vero
+          quas eum dolorem voluptates laboriosam? Dolor neque quas minus numquam
+          laboriosam magni voluptatem enim eveniet.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          dolor accusamus neque laborum atque accusantium perspiciatis, in illo
+          itaque assumenda totam. Facilis, voluptatum ipsum excepturi similique
+          facere unde quos necessitatibus quia culpa eligendi consequuntur vero
+          quas eum dolorem voluptates laboriosam? Dolor neque quas minus numquam
+          laboriosam magni voluptatem enim eveniet.
+        </p>
+      </main>
+    </div>
+  );
+}
